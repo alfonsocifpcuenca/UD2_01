a732704dca6d66ac53cf10e32b8ebdb665c5125a
alfonsocifpcuenca <alfonso.rodriguez@cifpcuenca.es>
2024-10-08 19:17:19 +0200

    Se añade chequeo de variables introducidas por el usuario

--- Ejercicios_UD2_1_1/Program.cs
+++ Ejercicios_UD2_1_1/Program.cs
@@ -7,11 +7,22 @@
             Console.WriteLine("Año de nacimiento: ");
             string anoNacimientoInput = Console.ReadLine();
 
+            int anoNacimiento;
+            if (int.TryParse(anoNacimientoInput, out anoNacimiento) == false)
+            { 
+                Console.WriteLine("El año introducido es incorrecto.");
+                return;            
+            }
+
             Console.WriteLine("Año actual: ");
             string anoActualInput = Console.ReadLine();
 
-            int anoNacimiento = int.Parse(anoNacimientoInput);
-            int anoActual = int.Parse(anoActualInput);
+            int anoActual;
+            if (int.TryParse(anoActualInput, out anoActual) == false)
+            {
+                Console.WriteLine("El año actual es incorrecto.");
+                return;
+            }
 
             Console.WriteLine($"Una persona nacida en {anoNacimiento} tendrá en {anoActual} {(anoActual - anoNacimiento)} años");            
         }
